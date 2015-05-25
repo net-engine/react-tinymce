b89a424292be2c3b4de862e4767d1b816770cc2d
Brad Parker <hi@bradparker.com>
2015-05-25 10:08:00 +1000

    Revert "Adds tinymce as dependency"
    This reverts commit 77f70769a8cc0c3f606498e6b3046160a901c306.

--- specs/TinyMCE.spec.js
+++ specs/TinyMCE.spec.js
@@ -1,10 +1,7 @@
 require('./helper');
 
-var container = document.createElement('div')
-
 describe('react-tinymce', function () {
   it('should render', function () {
-    var component = React.render(<TinyMCE />, container)
-    equal(!!component, true)
+    equal(true, true);
   });
 });
